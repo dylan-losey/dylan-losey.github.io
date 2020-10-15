482c92436914215e32828742abdab006faca49e8
dylan-losey <losey@vt.edu>
2020-10-15 18:04:47 -0500

    forms for applications

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202008142321",
+        "/css/pixyll.css?202010151804",
         "/"
       ]);
     })
