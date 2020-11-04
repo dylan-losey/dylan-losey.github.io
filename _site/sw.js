f4de93666416ea55b1eb5601afbff6548cbf4926
dylan-losey <losey@vt.edu>
2020-11-04 11:18:28 -0500

    move talks to collab

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202010151804",
+        "/css/pixyll.css?202011041118",
         "/"
       ]);
     })
